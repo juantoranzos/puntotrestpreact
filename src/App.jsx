b2cbb718018eb4b0c3430aebf4c4a5a6c442775d
juantoranzos <toranzos628@gmail.com>
2023-05-22 13:56:12 -0300

    limpieza de plantilla

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+
+
+  return (
+    <>
+    <HelloWorld></HelloWorld>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const HelloWorld = (props) => {
+  const [msj, setMsj] = useState('');
+
+  const handleClick = () => {
+    setMsj('(from changed state)');
+  }
+
+  return (
+    <>
+      <h1>Hello {props.name} {msj}</h1>
+      <button onClick={handleClick}>Change State</button>
+    </>
+  );
+}
+
+export default HelloWorld;
